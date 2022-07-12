eb79de6c714782067139b569e0a1d594dd3384bc
katie10147 <67284971+katie10147@users.noreply.github.com>
2022-07-12 10:27:51 -0400

    Create AccessingProjectSettingsFromFrontEnd.cs

---
+++ AccessingProjectSettingsFromFrontEnd.cs
@@ -0,0 +1,26 @@
+1. 
+
+            if (UmiContext.Current != null)
+            {
+                if (UmiContext.Current.ModuleProjectSettings.Get<ExtraEnergyProjectSettings>() != null)
+                {
+                    var projectSettings = UmiContext.Current.ModuleProjectSettings.Get<ExtraEnergyProjectSettings>();
+                    return projectSettings.StreetLightingByLayer;
+                }
+            }
+
+2. 
+
+
+	public object? StreetInformation1 => UmiContext.Current != null ? UmiContext.Current.ModuleProjectSettings.Get<ExtraEnergyProjectSettings>().StreetLightingByLayer : default(object?);
+
+
+3. 
+
+	private void RunGetStreetLengthsCommand()
+        {
+            RhinoApp.RunScript("UmiGetStreetLengths", echo: true);
+            var projectSettings = UmiContext.Current.ModuleProjectSettings.Get<ExtraEnergyProjectSettings>();
+            streetInformation = projectSettings.StreetLightingByLayer;
+
+        }
